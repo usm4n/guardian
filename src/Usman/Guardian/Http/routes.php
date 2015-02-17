f67e5b429e1610515782e8b632ac210104a80f0d
Usman Riaz <usmanri4z@gmail.com>
2015-02-18 00:28:08 +0500

    enabled auth middleware

--- src/Usman/Guardian/Http/routes.php
+++ src/Usman/Guardian/Http/routes.php
@@ -3,7 +3,7 @@
 Route::group([
     'prefix'=>'guardian/backend',
     'namespace'=>'Usman\Guardian\Http\Controllers',
-    //'middleware' => 'auth'
+    'middleware' => 'auth'
     ], 
     function()
     {    
